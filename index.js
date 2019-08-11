b63920e68b8a8ddfdd990f8680424e5dac7f4d4a
Oliver Gould <beek@protonmail.com>
2019-08-11 12:12:23 -0700

    initial import

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const CoinbasePro = require('coinbase-pro');
+
+
+const websocket = new CoinbasePro.WebsocketClient(
+    ['BTC-USD'],
+    process.env.SANDBOX_URL,
+    {
+        key: process.env.SANDBOX_KEY,
+        secret: process.env.SANDBOX_SECRET,
+        passphrase: process.env.SANDBOX_PASSPHRASE
+    },
+    { channels: ['hearbeat'] }
+    );
+
+websocket.on('message', data => {
+  console.log('message');
+  console.log(data);
+});
+websocket.on('error', err => {
+    console.log('error');
+    console.log(err);
+});
+websocket.on('close', () => {
+    console.log('close');
+});
